fd7e361d3ebe9107e10c9993a795fda2d32b54b0
Alen Saini <alensaini927@gmail.com>
2026-03-17 14:30:50 +0530

    Add files via upload

---
+++ number_gusseing_game.py
@@ -0,0 +1,14 @@
+import random
+
+number = random.randint(1,10)
+
+while True:
+    guess = int(input("Guess a number between 1 and 10: "))
+
+    if guess < number:
+        print("Too Low ⬇️")
+    elif guess > number:
+        print("Too High ⬆️")
+    else:
+        print("Correct 🎉")
+        break
